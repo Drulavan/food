4b7cfbda1fe7a8ad3ed6313bb70fa0badc5917a5
swamp1820 <swamp1820@yandex.ru>
2020-06-19 11:56:24 +0300

    add console app

---
+++ FoodBot/FoodBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
